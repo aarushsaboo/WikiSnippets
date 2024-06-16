84075b509d4b673b15c3e0880fc91f1fc286d436
Aarush Saboo <160993404+aarushsaboo@users.noreply.github.com>
2024-06-16 23:15:08 +0530

    Add files via upload

---
+++ v2_display_search_results.py
@@ -0,0 +1,65 @@
+import asyncio
+from playwright.async_api import Playwright, async_playwright, expect
+import os
+import streamlit as st
+paragraphs=[]
+async def run(playwright: Playwright, search_term: str) -> None:
+    global paragraphs
+    browser = await playwright.chromium.launch(headless=True)
+    context = await browser.new_context()
+    page = await context.new_page()
+    await page.goto("https://www.wikipedia.org/")
+    await page.get_by_role("link", name="English 6,796,000+ articles").click()
+    await page.get_by_placeholder("Search Wikipedia").click()
+    await page.get_by_placeholder("Search Wikipedia").fill(search_term)
+    try:
+        await page.locator("//li[@id='cdx-menu-item-1']").click()
+    except:
+        paragraphs = []
+        await context.close()
+        await browser.close()
+        return
+
+    # sometimes my paragraphs.txt contains only 3 entries instead of the 41 entries it is supposed to contain
+    # The issue you're encountering is likely due to the page not being fully loaded or the elements not being fully rendered when you attempt to retrieve the paragraphs. 
+    print('Is this printed 2222')
+    await page.wait_for_load_state("networkidle")
+
+    # Retrieve and print the text content of all paragraphs
+    paragraphs = await page.locator('p').all_text_contents()
+    
+    for paragraph in paragraphs:
+        print(paragraph)
+
+    with open('wikipedia/wiki_paragraphs.txt', 'w', encoding='utf-8') as f:
+        f.write('\n\n'.join(paragraphs))
+
+    await context.close()
+    await browser.close()
+
+async def main(search_term: str) -> None:
+    async with async_playwright() as playwright:
+        await run(playwright, search_term)
+
+def run_async_task(search_term: str):
+    loop = asyncio.ProactorEventLoop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main(search_term))
+
+# run_async_task()
+
+st.title('Dive into Wikipedia: Quick Insights!')
+search_term = st.text_input('Enter a topic to search on Wikipedia')
+if st.button('Search'):
+    with st.spinner('Searching Wikipedia...'):
+        run_async_task(search_term)
+    if paragraphs:
+        st.success("Here are the insights we found:")
+        if len(paragraphs) > 1:
+            st.write(paragraphs[1])
+        if len(paragraphs) > 2:
+            st.write(paragraphs[2])
+        if len(paragraphs) > 3:
+            st.write(paragraphs[3])
+    else:
+        st.write('Please search for something more generic')
